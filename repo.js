4f0c0bb83c86a180e6da407788f7716e07a867d2
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 13:34:49 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -281,3 +281,50 @@ function new_game(){
     resources['people']['unemployed'] = resources['people']['amount'];
     document.getElementById('unemployed-workers').textContent = resources['people']['unemployed'];
 }
+
+function repo_init(){
+    core_repo_init({
+      'beforeunload': {
+        'todo': function(){
+            return 'Game does not yet save.';
+        },
+      },
+      'globals': {
+        'daylight_passed': 0,
+        'resource_defaults': {
+          'food': {
+            'amount': 10,
+            'bonus': -1,
+            'multiplier': 2,
+          },
+          'gold': {},
+          'people': {
+            'amount': 1,
+          },
+          'stone': {},
+        },
+        'resources': {},
+        'start_new_day': '<input onclick=new_day() type=button value="Start New Day [ENTER]">',
+      },
+      'keybinds': {
+        13: {
+          'todo': function(){
+              if(daylight_passed === 0
+                && resources['people']['amount'] > 0){
+                  new_day();
+              }
+          },
+        },
+      },
+      'storage': {
+        'day-event-duration': 500,
+        'day-events': 10,
+      },
+      'storage-menu': '<table><tr><td><input class=mini id=day-event-duration min=1 step=any type=number><td>Event Duration'
+        + '<tr><td><input class=mini id=day-events min=1 step=any type=number><td>Events/Day</table>',
+      'title': 'Hrad.htm',
+    });
+
+    core_storage_update();
+    new_game();
+}
